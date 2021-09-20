b8aec1e87040b297766cc92782145e3229e09f16
qqkoh <qianqikoh@protonmail.com>
2021-09-20 12:57:26 +0800

    Print data using streams

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -39,6 +39,12 @@ public class Main {
         }
     }
 
+    public static void printDataWithStreams(ArrayList<Task> tasksData) {
+        System.out.println("Printing data using stream: ");
+        tasksData.stream() // convert data to stream
+                .forEach(System.out::println); // terminal operator
+    }
+
     public static void printDeadlines(ArrayList<Task> tasksData) {
         for (Task t : tasksData) {
             if (t instanceof Deadline) {
